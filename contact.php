8ceadbf6de23a35990b6a1d610e391ba70406788
verma-JEET <101511437+verma-JEET@users.noreply.github.com>
2022-03-14 01:00:52 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,25 @@
+
+<html>
+<head>
+<title>Phone Book</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body style="background-image: url('wall100.jpg');
+                  background-size:cover;">
+<div id = "main">
+  <h1> Phone Book</h1>
+  <?php 
+  
+  include_once 'menu-main.php';
+  ?>
+  
+<p class="cpara"></p>
+<h2 class="chead">Phone No. :- +91 9876543210</h2>
+<p class="cpara"></p> 
+ <h2 class="chead">Whatsapp No. :- +91 1234567890</h2>
+<p class="cpara"></p> 
+  <h2 class="chead">Contact me Via E-mail :- jeet12345@gmail.com</h2>
+<p class="cpara"></p>
+</div>
+</body>
+</html>
